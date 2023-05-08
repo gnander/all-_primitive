300c72f558eec53e0000533335db734e740ff42e
gnanu <morlagnanu@gmail.com>
2023-05-08 23:53:53 +0530

    val

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
-    static boolean val1;
-    static double val2;
-    static float val3;
-    static int val4;
-    static long val5;
-    static String val6;
+    static boolean False;
+    static double 0.0d;
+    static float 0.0f;
+    static int 0;
+    static long 0L;
+    static String val1;
     public static void main(String[] args) {
         System.out.println("Default values.....");
-        System.out.println("Val1 = " + val1);
-        System.out.println("Val2 = " + val2);
-        System.out.println("Val3 = " + val3);
-        System.out.println("Val4 = " + val4);
-        System.out.println("Val5 = " + val5);
-        System.out.println("Val6 = " + val6);
+        System.out.println("Val1 = " + False);
+        System.out.println("Val2 = " + 0.0d);
+        System.out.println("Val3 = " + 0.0f);
+        System.out.println("Val4 = " + 0);
+        System.out.println("Val5 = " + 0L);
+        System.out.println("Val6 = " + val1);
     }
 }
